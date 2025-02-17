3668affff8e100d1679229e059897f56be6e272f
cibaer1 <ivoleonidalbert@gmail.com>
2025-02-17 22:19:14 +0100

    baked NavMesh and added basic pathfinding to Chicken

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -1,7 +1,29 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Chicken : MonoBehaviour
 {
-    [SerializeField]float moveSpeed = 1f;
-    
+    public NavMeshAgent agent;
+    public float viewRadius;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+        
+    }
+    private void Update()
+    {
+        
+        float dist = Vector3.Distance(agent.destination, transform.position);
+        if(dist < 0.5f)
+        {
+            float x = Random.Range(viewRadius * -0.5f, viewRadius * 0.5f);
+            float y = Random.Range(viewRadius * -0.5f, viewRadius * 0.5f);
+            Vector3 randomDest = new Vector3(transform.position.x - x, 0.51f, transform.position.y - y);
+            agent.SetDestination(randomDest);
+            Debug.Log(randomDest);
+        }
+    }
+
 }
